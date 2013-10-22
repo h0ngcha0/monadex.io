4eb8d4cdfd0b2fd50b6bfe67c3a07df69f175fe9
hongchao <lbs.lhc@gmail.com>
2013-10-22 21:23:13 +0200

    add enlisted html

---
+++ js/enlisted.js
@@ -0,0 +1,10 @@
+$(function readFunction() {
+  /* smooth-scroll */
+  $("ul.navbar-nav a").smoothScroll({
+    offset: 30,
+    afterScroll: function () {
+      $('ul.navbar-nav ul a').removeClass('active');
+      $(this).addClass('active');
+    }
+  });
+});
